0cc29bfde9633cc225458aad1fb5ceccb18d2358
thisiskeshavyadavv@gmail.com <Keshav yadav>
2024-07-02 21:31:39 +0530

    NORTERA

---
+++ script.js
@@ -0,0 +1,39 @@
+gsap.from("#nav #logo,#nav #navcenter,#nav #navright , #homeleft",{
+    y : -200,
+    duration:1,
+    delay:1,
+    opacity:0,
+    stagger:0.2
+
+}
+
+)
+gsap.from("#homeright, #homedown,#secondphoto1,#secondphoto2",{
+    y: 400,
+    duration:1,
+    delay:1,
+    opacity:0,
+    stagger:0.5
+
+}
+
+)
+
+// console.log("kesh");
+// vara a;
+// console.log("yadav");
+
+// avar p;
+// console.log("kehgaf")
+
+// console.log("a");
+var a;
+// b/c in a i have undefined and undefined is an falsy value
+
+if(a){
+    console.log("kesh");
+}
+
+else {
+    console.log("else");
+}
